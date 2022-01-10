95b9bf93825f032260df543863cd3060c52551e8
Brad Babb <brad_babb@cable.comcast.com>
2022-01-09 21:53:59 -0600

    updates

---
+++ src/lib/ea.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import Icon from '@iconify/svelte';
+	export let text: string = null;
+	export let url: string = null;
+    export let alt: string = null;
+
+	let realText = text || url || 'UNKNOWN';
+    let realAlt = alt || realText
+</script>
+
+<a href={url} target="_blank" alt={realAlt} class="text-slate-600 hover:text-slate-800">
+    <div class="inline-flex flex-row align items-center">
+        <span class="pr-0.5">{realText}</span>
+        <Icon width="1rem" height="1rem" icon="fa-solid:external-link-square-alt" />
+    </div>
+</a>
---
+++ src/routes/index.svelte
@@ -0,0 +1,6 @@
+<script>
+  import "../app.css";
+  import Ea from '$lib/ea.svelte'
+</script>
+
+<Ea url="http://www.github.com" />
